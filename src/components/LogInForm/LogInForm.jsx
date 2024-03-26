ab24e0d89ca414b6061fa676ce14251de9f63323
NZAlona <amakoveychenko@gmail.com>
2024-03-26 16:15:54 +0200

    Adds form validation schema

--- src/components/LogInForm/LogInForm.jsx
+++ src/components/LogInForm/LogInForm.jsx
@@ -1,7 +1,19 @@
 import { Field, Formik, Form } from 'formik';
 import css from './LoginForm.module.css';
+import * as Yup from 'yup';
+import { ErrorMessage } from 'formik';
+import { Link } from 'react-router-dom';
+
+const ValidationSchema = Yup.object().shape({
+  useremail: Yup.string().email('Must be a valid email!').required('Required'),
+  userpassword: Yup.string().min(3, 'Too short').max(256, 'Too long').required('Required'),
+});
 
 export default function LongInForm() {
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
   return (
     <div className={css.container}>
       <h2 className={css.text}>Log In</h2>
@@ -11,20 +23,29 @@ export default function LongInForm() {
             useremail: '',
             userpassword: '',
           }}
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
+          validationSchema={ValidationSchema}
         >
           <Form className={css.forma}>
             <label htmlFor="useremail" className={css.formLabel}>
               Email
             </label>
             <Field type="text" name="useremail" className={css.formInput} />
+            <ErrorMessage name="useremail" component="span" className={css.error} />
             <label htmlFor="userpassword" className={css.formLabel}>
               Password
             </label>
-            <Field type="text" name="userpassword" className={css.formInput} />
+            <Field type="password" name="userpassword" className={css.formInput} />
+            <ErrorMessage name="userpassword" component="span" className={css.error} />
             <button type="submit" className={css.btn}>
               Log In
             </button>
+            <p className={css.ptext}>
+              Do not have an account?
+              <Link to="/register" className={css.link}>
+                Register
+              </Link>
+            </p>
           </Form>
         </Formik>
       </span>
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,7 +1,20 @@
 import { Field, Form, Formik } from 'formik';
+import { Link } from 'react-router-dom';
 import css from './RegisterForm.module.css';
+import * as Yup from 'yup';
+import { ErrorMessage } from 'formik';
+
+const ValidationSchema = Yup.object().shape({
+  username: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+  useremail: Yup.string().email('Must be a valid email!').required('Required'),
+  password: Yup.string().min(3, 'Too short').max(256, 'Too long').required('Required'),
+});
 
 export default function RegisterForm() {
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
   return (
     <>
       <div className={css.wrapper}>
@@ -13,24 +26,34 @@ export default function RegisterForm() {
               useremail: '',
               password: '',
             }}
-            onSubmit={() => {}}
+            onSubmit={handleSubmit}
+            validationSchema={ValidationSchema}
           >
             <Form className={css.forma}>
               <label htmlFor="username" className={css.formLabel}>
                 Username
               </label>
               <Field type="text" name="username" className={css.formInput} />
+              <ErrorMessage name="username" component="span" className={css.error} />
               <label htmlFor="useremail" className={css.formLabel}>
                 Email
               </label>
               <Field type="text" name="useremail" className={css.formInput} />
+              <ErrorMessage name="useremail" component="span" className={css.error} />
               <label htmlFor="password" className={css.formLabel}>
                 Password
               </label>
-              <Field type="text" name="password" className={css.formInput} />
+              <Field type="password" name="password" className={css.formInput} />
+              <ErrorMessage name="password" component="span" className={css.error} />
               <button type="submit" className={css.btn}>
                 Register
               </button>
+              <p className={css.ptext}>
+                Already have an account?
+                <Link to="/login" className={css.link}>
+                  Log In
+                </Link>
+              </p>
             </Form>
           </Formik>
         </span>
