5291419fed8b7a3cfd1a8ae074049ed68350d94b
NZAlona <amakoveychenko@gmail.com>
2024-04-03 18:34:45 +0300

    Adds updated imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import NotFoundPage from './pages/NotFoundPage';
-import LogIn from './pages/LogIn';
-import Register from './pages/Register';
-import Contacts from './pages/Contacts';
+// import LogIn from './pages/LogIn';
+// import Register from './pages/Register';
+// import Contacts from './pages/Contacts';
 import Layout from './components/Layout/Layout';
 import { useDispatch, useSelector } from 'react-redux';
-import { Suspense, useEffect } from 'react';
+import { Suspense, useEffect, lazy } from 'react';
 import { refreshUser } from './redux/auth/operations';
 import { selectIsRefreshing } from './redux/auth/authSelectors';
 import RestrictedRoute from './RestrictedRoute';
 import PrivateRoute from './PrivateRoute';
+
+const Home = lazy(() => import('./pages/Home'));
+const Register = lazy(() => import('./pages/Register'));
+const LogIn = lazy(() => import('./pages/LogIn'));
+const Contacts = lazy(() => import('./pages/Contacts'));
 
 export default function App() {
   const dispatch = useDispatch();
